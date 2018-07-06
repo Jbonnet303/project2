51ce674f7533a2548e0b9d410d0e708f0e351eee
Jon Bonnet <jbonnet303@gmail.com>
2018-07-06 11:57:50 -0400

    mongoose installed and parser added

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+const mongoose = require('mongoose');
+const mongoUri =  process.env.MONGODB_URI || 'mongodb://localhost:27017/grocery_app_dev';
+
 app.get('/', (request, response) => {
   response.send('this works');
 });
@@ -11,3 +15,9 @@ app.get('/', (request, response) => {
 app.listen(PORT, () => {
   console.log('listening...');
 });
+
+
+mongoose.connect(mongoUri, { useNewUrlParser: true });
+mongoose.connection.on('open', () => {
+  console.log('connected to mongoose!!!!!!!');
+})
