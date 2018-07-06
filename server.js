003afbceee6a980ab5d8c07c53c23b8cd437c4b2
Jon Bonnet <jbonnet303@gmail.com>
2018-07-06 10:47:11 -0400

    express running

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log('Hello');
+// console.log('Hello');
+const express = require('express');
+const app = express();
+
+app.get('/', (request, response) => {
+  response.send('this works');
+});
+
+
+app.listen(3000, () => {
+  console.log('listening...');
+});
