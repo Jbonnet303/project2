c7928907ce904e78863d47c322f73e6eb16fb884
Jon Bonnet <jbonnet303@gmail.com>
2018-07-06 11:32:36 -0400

    port setup to heroku

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 // console.log('Hello');
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (request, response) => {
   response.send('this works');
 });
 
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('listening...');
 });
